cfc27df8f22bd738ea3c1dc26f0b033c209c3e90
Danya Generalov <danya@danya02.ru>
2022-09-10 04:17:58 +0300

    Adding project view and name+description editor.

--- src/projects.py
+++ src/projects.py
@@ -1,7 +1,9 @@
-from flask import Blueprint, g, render_template, redirect, url_for
+from flask import Blueprint, g, render_template, redirect, url_for, abort, flash
 from utils import require_login
 from database import *
 from flask_wtf import FlaskForm
+from wtforms import StringField, TextAreaField
+from wtforms.validators import DataRequired
 
 
 bp = Blueprint('projects', __name__, url_prefix='/projects')
@@ -29,7 +31,46 @@ def create():
         project = Project.create(owner=g.user, name='New Untitled Project')
         return redirect(url_for('projects.view', slug=project.slug))
 
+class NameEditForm(FlaskForm):
+    name = StringField('Name', validators=[DataRequired()])
+
+class DescriptionEditForm(FlaskForm):
+    description = TextAreaField('Description')
+
 @bp.route('/<slug>')
 @require_login
 def view(slug):
-    return f'Project {slug}'
+    project = Project.get_or_none(Project.slug == slug)
+    if project is None:
+        return abort(404)
+    name_edit_form = NameEditForm(name=project.name)
+    description_edit_form = DescriptionEditForm(description=project.description)
+    return render_template('dashboard-project-view.html', project=project, name_edit_form=name_edit_form, description_edit_form=description_edit_form)
+
+@bp.route('/api/<slug>/edit-name', methods=['POST'])
+@require_login
+def api_update_name(slug):
+    project = Project.get_or_none(Project.slug == slug)
+    if project is None:
+        return abort(404)
+    form = NameEditForm()
+    if form.validate_on_submit():
+        project.name = form.name.data
+        project.save()
+        flash('project-name-updated', 'success')
+    else:
+        flash('project-name-update-error', 'danger')
+    return redirect(url_for('projects.view', slug=project.slug))
+
+@bp.route('/api/<slug>/edit-description', methods=['POST'])
+@require_login
+def api_update_description(slug):
+    project = Project.get_or_none(Project.slug == slug)
+    if project is None:
+        return abort(404)
+    form = DescriptionEditForm()
+    if form.validate_on_submit():
+        project.description = form.description.data
+        project.save()
+        flash('description-updated', 'success')
+        return redirect(url_for('projects.view', slug=project.slug))
